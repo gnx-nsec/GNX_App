b771f4eef08f59238b197cd1591f8c35eada4a43
Faruque Aziz <faruqueaziz611@gmail.com>
2020-08-14 22:55:31 +0530

    working on about us

--- lib/navDrawerElement/aboutUs.dart
+++ lib/navDrawerElement/aboutUs.dart
@@ -13,6 +13,30 @@ class AboutUSState extends State<AboutUS> {
         backgroundColor: Color(0xffff6f00),
         title: Text("About Us"),
       ),
+      body: Center(
+        child: Stack(
+          children: <Widget>[
+            // Top red container
+            Container(
+              alignment: Alignment.topLeft,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.25,
+              color: Color(0xffff6f00),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text(
+                  'About Us',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 26.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
